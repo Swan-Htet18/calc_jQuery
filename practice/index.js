132d54691871cb519f0f356c8e901120349fb2be
Swann Htet <swannhtet@Swanns-MacBook-Air.local>
2022-12-26 12:17:26 +0630

    1st commit

---
+++ practice/index.js
@@ -0,0 +1,14 @@
+$('#date').datepicker({
+    showOtherMonths:true,
+    selectOtherMonths:true,
+    showButtonPanel:true,
+    changeMonth:true,
+    changeYear:true,
+    showWeek:true
+});
+
+$('img').tooltip({
+    track:true,
+    show:{effect:"slideDown",duration:500},
+    hide:{effect:"slideUp",duration:500}
+});
